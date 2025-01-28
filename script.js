e6b88a6ef7239c25c5e33daf2a1e5d6fb7150ac5
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-28 18:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ function getSize() {
+            const width = document.documentElement.clientWidth;
+            const height = document.documentElement.clientHeight;
+            document.getElementById('sizeInfo').innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+        }
+        window.addEventListener("resize", getSize);
